7e927896250663793d823b9ae896482a5921003f
1lexaa <nice140@icloud.com>
2024-03-29 00:50:11 +0200

    !!!

--- components/intro.tsx
+++ components/intro.tsx
@@ -87,7 +87,7 @@ export default function Intro() {
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-100 hover:scale-105 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           //className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-100 hover:bg-gray-950 active:scale-105 transition"
-          href="/CV.zip"
+          href="/cv.pdf"
           download
         >
           Download CV{" "}
