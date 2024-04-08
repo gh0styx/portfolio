3b9f2474aa7e3919e5d79dc6dcfc2b78ed69ac47
1lexaa <nice140@icloud.com>
2024-04-08 20:30:33 +0300

    gogog

--- components/intro.tsx
+++ components/intro.tsx
@@ -87,11 +87,9 @@ export default function Intro() {
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-100 hover:scale-105 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           //className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-100 hover:bg-gray-950 active:scale-105 transition"
-          href="/cv.pdf"
+          href="/cv.zip"
           download
         >
-
-          
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
